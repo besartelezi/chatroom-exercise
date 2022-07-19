ccd89bafc6469c06f9f282e5e0d48ad6c3e99c8f
Besart Elezi <besart.elezi@hotmail.com>
2022-07-19 14:17:57 +0200

    fixed increasing messages bug

--- client/scripts.js
+++ client/scripts.js
@@ -3,17 +3,20 @@ let socket = io.connect();
 //where all messages are shown
 const targetMessages = document.getElementById('allMessagesBox');
 //message that user inputs
-let message = document.getElementById('messageBox').value;
 
 //shows all messages
-//there is currently a bug, the first message the user has input is the
-function showMessage () {
-    socket.emit('sendToAll', (message));
-    socket.on('displayMessage', (message) => {
-        targetMessages.innerHTML += '<br>'+message;
+//there is currently a bug, the messages keep incrementing
+function sendMessage () {
+    let message = document.getElementById('messageBox').value;
+    socket.emit('sendToAll', (message ));
+    //socket.on kept incrementing the amount of times it was activated
+    //I changed it to socket.once, so it will be called upon just once
+    socket.once('displayMessage', (message) => {
+        console.log(message)
+        targetMessages.innerHTML += '<br>'+ message;
     });
 }
 
 //defines send to all button and adds event listener
 const sendAllButton = document.getElementById('sendAll');
-sendAllButton.addEventListener("click", showMessage);
+sendAllButton.addEventListener("click", sendMessage);
