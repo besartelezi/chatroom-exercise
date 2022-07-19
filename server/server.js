93920f127076b7f92d6a46ecb67f03e413c779de
Besart Elezi <besart.elezi@hotmail.com>
2022-07-19 09:56:50 +0200

    added code to set server live

--- server/server.js
+++ server/server.js
@@ -1,2 +1,18 @@
+//requiring express and http to make it easier to host our client
 const express = require('express');
 const http = require('http');
+
+//defining the application
+const app = express();
+//This gives the path to our client
+const clientPath =`${__dirname}/../client/`;
+
+//uses express to host the client
+app.use(express.static(clientPath));
+//use http to serve the app that express provides
+const server = http.createServer(app);
+
+//sets the server live
+server.listen(8080, () =>{
+    console.log("server running on "+8080);
+});
